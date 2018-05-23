282818fb34ea00c55d4a123d90d7854bf97331bb
intelligentgamer <39501953+intelligentgamer@users.noreply.github.com>
2018-05-23 23:24:15 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,7 +1,7 @@
 const Discord = require ("discord.js")
 const client = new Discord.Client()
 var prefix = "ub!!";
-client.login("NDQ4NTc5NDQwMTU5NzUyMTky.Ded6Sw.yg3IlLXlGc9EtJ86L7v7llC668U")
+client.login("NDQ4NTc5NDQwMTU5NzUyMTky.Ded7aw.iThaCX0gnYCGkp61hsevRCXSatU")
 
 client.on("ready", function() {
     client.user.setActivity("ub!!", {type:"PLAYING"})
